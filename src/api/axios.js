3ad8240a150c19eb398720e63cd33fcf819de156
obscuredecode-93 <anurag.l.iyer@gmail.com>
2020-04-11 01:58:02 -0500

    Ready for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -17,5 +17,5 @@
 
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://thawing-shore-89996.herokuapp.com/",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://thawing-shore-89996.herokuapp.com/users/",
 });
